390ac78b0ebffba8860b6b8410e2e9d6828eca62
Amarvin <anthony@glacial.com>
2021-05-11 17:09:27 -0400

    Implement Sass-bootstrap pipeline

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -5,13 +5,16 @@ rules.push({
 	test: /\.css$/,
 	use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
-
+rules.push({
+	test: /\.(scss|css)$/,
+	use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+});
 module.exports = {
 	module: {
 		rules,
 	},
 	plugins: plugins,
 	resolve: {
-		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss'],
 	},
 };
--- webpack.rules.js
+++ webpack.rules.js
@@ -24,21 +24,4 @@ module.exports = [
 			},
 		},
 	},
-	{
-		test: /\.s[ac]ss$/i,
-		use: [
-			// Creates `style` nodes from JS strings
-			'style-loader',
-			// Translates CSS into CommonJS
-			'css-loader',
-			// Compiles Sass to CSS
-			{
-				loader: 'sass-loader',
-				options: {
-					// Prefer `dart-sass`
-					implementation: require('sass'),
-				},
-			},
-		],
-	},
 ];
