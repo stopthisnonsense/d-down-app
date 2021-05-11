fdad6477f9ef34cfd8c17520f4b4ab3c3b9e5126
Amarvin <anthony@glacial.com>
2021-05-11 18:02:24 -0400

    Sass Port!

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function Welcome() {
     return(
-        <div>
+        <div className="container-md">
             <h1>🍆 Welcome to D-Down! 🍆</h1>
+            <div className='row'>
+                <div className="col-6"><button className="btn btn-secondary">Sign Up</button></div>
+                <div className="col-6"><button className="btn btn-light">Sign In</button></div>
+
+            </div>
         </div>
+
 
     )
 }
